e7e78cc6103f19da32db92719ea901125cd49997
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-11 04:38:48 +0900

    [feat] 모듈 적용

--- server/src/main.ts
+++ server/src/main.ts
@@ -15,6 +15,9 @@ async function bootstrap() {
                   .setTitle('여행갈래 api docs')
                   .setDescription('여행갈래 앱 Api 문서입니다.')
                   .setVersion('0.1')
+                  .addBearerAuth({
+                    type: 'http', scheme: 'bearer', bearerFormat: 'Token'
+                  }, 'access-token')
                   .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
