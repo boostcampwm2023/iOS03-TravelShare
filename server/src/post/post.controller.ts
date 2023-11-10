797449c768e98570fcfc7d92eceb71081f84fcb8
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-11 04:38:32 +0900

    [refactor] post controller dto 이름 수정 반영

--- server/src/post/post.controller.ts
+++ server/src/post/post.controller.ts
@@ -1,23 +1,25 @@
 import { Controller, Get, Post, Query } from "@nestjs/common";
-import { ApiBearerAuth, ApiBody, ApiHeader, ApiQuery, ApiResponse } from "@nestjs/swagger";
-import { PostFindResponse } from "./post.find.response";
-import { PostReadResponse } from "./post.read.response";
-import { PostWriteRequest } from "./post.write.request";
+import { ApiBearerAuth, ApiBody, ApiQuery, ApiResponse } from "@nestjs/swagger";
+import { PostFindResponse } from "./post.find.response.dto";
+import { PostReadResponse } from "./post.read.response.dto";
+import { PostWriteBody } from "./post.write.request";
+import { PostFindQuery } from "./post.find.query.dto";
 
-@ApiHeader({name: 'Authorization', description: 'JWT', schema: {type: 'JWT'}})
+@ApiBearerAuth('access-token')
 @Controller('post')
 export class PostController {
 
     @ApiResponse({description: '리스트 조회', type: [PostFindResponse]})
+    @ApiQuery({description: ''})
     @Get('find')
-    async find() {}
+    async find(@Query() query: PostFindQuery) {}
 
     @ApiResponse({description: '상세 조회', type: PostReadResponse})
     @ApiQuery({description: '게시글 id'})
     @Get('detail')
     async detail(@Query('id') id: number) {}
 
-    @ApiBody({description: '업로드', type: PostWriteRequest})
+    @ApiBody({description: '업로드', type: PostWriteBody})
     @Post('upload')
     async upload() {}
 }
