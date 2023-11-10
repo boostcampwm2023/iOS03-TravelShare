d705705827b3ac7baab1ae2e811448aa606a2539
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-10 20:09:39 +0900

    [feat] Nest.js 프로젝트 생성

---
+++ server/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'fatal', 'warn', 'log', 'verbose'],
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+                  .setTitle('여행갈래 api docs')
+                  .setDescription('여행갈래 앱 Api 문서입니다.')
+                  .setVersion('0.1')
+                  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
