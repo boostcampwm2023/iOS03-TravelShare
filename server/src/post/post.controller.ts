950ec8187333b14cbbc12be7f77584334c5ad939
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-11 18:11:35 +0900

    [feat] api 추가 및 수정
    /post/like: 게시

--- server/src/post/post.controller.ts
+++ server/src/post/post.controller.ts
@@ -1,25 +1,40 @@
-import { Controller, Get, Post, Query } from "@nestjs/common";
-import { ApiBearerAuth, ApiBody, ApiQuery, ApiResponse } from "@nestjs/swagger";
-import { PostFindResponse } from "./post.find.response.dto";
-import { PostReadResponse } from "./post.read.response.dto";
-import { PostWriteBody } from "./post.write.request";
-import { PostFindQuery } from "./post.find.query.dto";
+import { Body, Controller, Get, Patch, Post, Query } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
+} from '@nestjs/swagger';
+import { PostFindResponse } from './post.find.response.dto';
+import { PostReadResponse } from './post.read.response.dto';
+import { PostWriteBody } from './post.write.body';
+import { PostFindQuery } from './post.find.query.dto';
 
 @ApiBearerAuth('access-token')
 @Controller('post')
 export class PostController {
+  @ApiResponse({ description: '리스트 조회', type: [PostFindResponse] })
+  @ApiQuery({ description: '' })
+  @Get('find')
+  async find(@Query() query: PostFindQuery) {}
 
-    @ApiResponse({description: '리스트 조회', type: [PostFindResponse]})
-    @ApiQuery({description: ''})
-    @Get('find')
-    async find(@Query() query: PostFindQuery) {}
+  @ApiResponse({ description: '상세 조회', type: PostReadResponse })
+  @ApiQuery({ description: '게시글 id' })
+  @Get('detail')
+  async detail(@Query('id') id: number) {}
 
-    @ApiResponse({description: '상세 조회', type: PostReadResponse})
-    @ApiQuery({description: '게시글 id'})
-    @Get('detail')
-    async detail(@Query('id') id: number) {}
+  @ApiBody({ description: '업로드', type: PostWriteBody })
+  @Post('upload')
+  async upload(@Body() post: PostWriteBody) {}
 
-    @ApiBody({description: '업로드', type: PostWriteBody})
-    @Post('upload')
-    async upload() {}
+  @ApiOperation({ description: '게시글 좋아요' })
+  @ApiQuery({ description: '게시글 id' })
+  @Patch('like')
+  async like(@Query('id') id: number) {}
+
+  @ApiOperation({ description: '게시글 좋아요 취소' })
+  @ApiQuery({ description: '게시글 id' })
+  @Patch('unlike')
+  async unlike(@Query('id') id: number) {}
 }
