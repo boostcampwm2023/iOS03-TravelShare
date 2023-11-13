5f922f6b1d64b0cf8256b7a2f4b18d012dddd5d9
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-13 23:01:58 +0900

    [add] TypeOrmModule 의존성 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostModule, UserModule],
+  imports: [
+    PostModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql',
+      port: 3306,
+      username: 'macro',
+      password: 'macro',
+      database: 'macro_dev_db',
+      migrations: ['dist/**/migrations/*.{js,ts}'],
+      entities: ['dist/**/entities/*.entity.{js,ts}'],
+      synchronize: true,
+      dropSchema: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
