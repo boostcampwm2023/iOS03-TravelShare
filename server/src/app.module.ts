e49a815ee7bc7499a5c070abff51b6bc76a5e8d0
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-14 16:33:40 +0900

    [feat] config module 설정
    Co-authored-by: NOSSOH <nossoh98@users.noreply.github.com>

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MapModule } from './map/map.module';
+import { AppConfigModule } from './config/app.config.module';
 
 @Module({
   imports: [
@@ -22,6 +24,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       dropSchema: true,
       logging: true,
     }),
+    AppConfigModule,
+    MapModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ server/src/config/app.config.ts
@@ -0,0 +1,7 @@
+import { readFileSync } from "fs"
+import { load } from "js-yaml"
+
+
+export default ()=> {
+    return load(readFileSync('application.secrets.yaml', 'utf-8')) as Record<string, any>;
+}
