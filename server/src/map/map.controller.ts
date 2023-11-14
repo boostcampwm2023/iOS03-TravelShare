3e49056a128ed45372278e6596b347c7fd50930e
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-14 16:34:35 +0900

    [feat] Map api 추가
    - /map/search: 키워드에 따른 장소 검색

---
+++ server/src/map/map.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Query } from "@nestjs/common";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { MapSearchResponse } from "./map.search.response.dto";
+import { MapService } from "./map.service";
+
+@Controller('map')
+export class MapController {
+    constructor(private readonly mapService: MapService) {}
+    
+    @ApiOperation({description: '주소나 이름에 따라 위치를 검색합니다.'})
+    @ApiResponse({type: [MapSearchResponse]})
+    @Get('search')
+    async search(@Query('keyword') keyword: string) {
+        return await this.mapService.search(keyword);
+    }
+}
+
