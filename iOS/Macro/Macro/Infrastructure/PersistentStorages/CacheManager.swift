552ee71d80b189e3a08315959b565bbf5a9faa1f
ykm989 <ykm989@naver.com>
2023-11-15 21:58:57 +0900

    [feat][#12] Cache Manager 구현

---
+++ iOS/Macro/Macro/Infrastructure/PersistentStorages/CacheManager.swift
@@ -0,0 +1,22 @@
+//
+//  CacheManager.swift
+//  Macro
+//
+//  Created by 김경호 on 11/15/23.
+//
+
+import Foundation
+
+final class CacheManager {
+    private let memoryCache = NSCache<NSString, NSObject>()
+}
+
+extension CacheManager {
+    public func save(key: String, data: NSObject) {
+        memoryCache.setObject(data, forKey: key as NSString)
+    }
+    
+    public func load(key: String) -> NSObject? {
+        return memoryCache.object(forKey: key as NSString)
+    }
+}
