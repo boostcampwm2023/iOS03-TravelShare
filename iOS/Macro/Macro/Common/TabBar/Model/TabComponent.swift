69da74b63fe355d995a9d3f4d9fe1a563e6a3903
Byeonjinha <jinhaday@gmail.com>
2023-11-15 23:09:11 +0900

    [feat][#15] CustomTab model 관련 작업

---
+++ iOS/Macro/Macro/Common/TabBar/Model/TabComponent.swift
@@ -0,0 +1,14 @@
+//
+//  TabComponent.swift
+//  Macro
+//
+//  Created by Byeon jinha on 11/14/23.
+//
+
+import Foundation
+
+struct TabComponent {
+    let index: Int
+    let imageName: String
+    let text: String
+}
---
+++ iOS/Macro/Macro/Common/TabBar/Model/TabLocation.swift
@@ -0,0 +1,13 @@
+//
+//  TabLocation.swift
+//  Macro
+//
+//  Created by Byeon jinha on 11/14/23.
+//
+
+import Foundation
+
+struct TabLocation {
+    let locationX: CGFloat
+    let locationY: CGFloat
+}
