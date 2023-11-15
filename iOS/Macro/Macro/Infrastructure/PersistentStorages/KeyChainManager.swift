fb01a68fbf900a7a64d08358d0a45734152a7e89
ykm989 <ykm989@naver.com>
2023-11-15 22:38:36 +0900

    [feat][#12] KeyChain Manager 구현

---
+++ iOS/Macro/Macro/Infrastructure/PersistentStorages/KeyChainManager.swift
@@ -0,0 +1,58 @@
+//
+//  File.swift
+//  Macro
+//
+//  Created by 김경호 on 11/15/23.
+//
+
+import Foundation
+
+struct KeyChainManager {
+    /// 키체인 데이터 저장 함수
+    static func save(key: String, token: String) {
+        if let data = token.data(using: .utf8) {
+            let query: [String: Any] = [
+                kSecClass as String: kSecClassGenericPassword,
+                kSecAttrAccount as String: key,
+                kSecValueData as String: data
+            ]
+            
+            let status = SecItemAdd(query as CFDictionary, nil)
+            
+            if status == errSecDuplicateItem {
+                let updateQuery: [String: Any] = [
+                    kSecClass as String: kSecClassGenericPassword,
+                    kSecAttrAccount as String: key
+                ]
+                
+                let attributes: [String: Any] = [
+                    kSecValueData as String: data
+                ]
+
+                SecItemUpdate(updateQuery as CFDictionary, attributes as CFDictionary)
+            }
+        }
+    }
+    
+    /// 키체인에서 데이터 검색 함수
+    static func load(key: String) -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: kCFBooleanTrue!,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        
+        var dataTypeRef: AnyObject?
+        
+        let status = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+        
+        if status == errSecSuccess, let data = dataTypeRef as? Data {
+            if let token = String(data: data, encoding: .utf8) {
+                return token
+            }
+        }
+        
+        return nil
+    }
+}
