17f8c08af377b36663475481a6d9491c3f9987d5
Byeonjinha <jinhaday@gmail.com>
2023-11-15 23:09:22 +0900

    [feat][#15] CustomTab viewModel 관련 작업

---
+++ iOS/Macro/Macro/Common/TabBar/ViewModel/TabBarViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  TabBarViewModel.swift
+//  Macro
+//
+//  Created by Byeon jinha on 11/14/23.
+//
+
+import Combine
+import Foundation
+
+final class TabBarViewModel {
+    let tabComponentArray = CurrentValueSubject<[TabComponent], Never>([
+        TabComponent(index: 1, imageName: "magnifyingglass", text: "주변 탐색"),
+        TabComponent(index: 2, imageName: "person.circle", text: "내 정보"),
+        TabComponent(index: 3, imageName: "square.and.pencil", text: "일지 작성"),
+        TabComponent(index: 4, imageName: "map", text: "여행")
+    ])
+    
+    let currentTabComponent = CurrentValueSubject<TabComponent, Never>(
+        TabComponent(index: 0, imageName: "house", text: "홈"))
+    
+    var radius: CGFloat = 90
+    var rotationAngle: Double = 0
+    var timer: Timer?
+    
+    var animationStep: Int = 0
+}
