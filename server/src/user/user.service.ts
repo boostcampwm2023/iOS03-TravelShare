3bf4b63998300ef36694e1f0eb9897527ad53222
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-20 23:36:45 +0900

    [move] 인증 로직 service 분리

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -2,46 +2,11 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
-import { UserSigninBody } from './user.signin.body.dto';
-import { UserSignupBody } from './user.signup.body.dto';
-import { UserDeleteBody } from './user.delete.body.dto';
-import { JwtService } from '@nestjs/jwt';
-import { compare, genSalt, hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-    private readonly jwtService: JwtService,
+    private readonly userRepository: Repository<User>
   ) {}
-
-  async signup(user: UserSignupBody) {
-    const userDetail = await this.userRepository.save({
-      ...user,
-      password: await hash(user.password, await genSalt()),
-    });
-    return await this.jwtService.signAsync({
-      email: userDetail.email,
-      role: userDetail.role,
-    });
-  }
-
-  async login(user: UserSigninBody) {
-    const userDetail = await this.userRepository.findOneOrFail({
-      where: { email: user.email },
-    });
-    if (await compare(user.password, userDetail.password)) {
-      return await this.jwtService.signAsync({
-        email: userDetail.email,
-        role: userDetail.role,
-      });
-    } else {
-      throw new UnauthorizedException('user not found');
-    }
-  }
-
-  async delete(user: UserDeleteBody) {
-    return await this.userRepository.delete({ email: user.email });
-  }
 }
