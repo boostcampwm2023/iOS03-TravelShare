bcb88bd09202f04430ac3f207aeb55fee347e096
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-20 23:32:18 +0900

    [rename & move] 인증 관련 로직을 분리하기 위한 dto 이동

--- server/src/user/user.delete.body.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail } from 'class-validator';
-
-export class UserDeleteBody {
-  @ApiProperty({ description: 'id email' })
-  @IsEmail()
-  email: string;
-}
