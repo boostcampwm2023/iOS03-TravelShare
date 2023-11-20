022bc6245945c86aabc7405bf6cf6a98474246d5
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-20 22:40:16 +0900

    [chore] [#12] dto 형식 수정 및 통일

--- server/src/user/user.signin.response.dto.ts
+++ server/src/user/user.signin.response.dto.ts
@@ -2,8 +2,8 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsJWT, IsNotEmpty } from 'class-validator';
 
 export class UserSigninResponse {
-  @ApiProperty({ description: 'jwt token' })
+  @ApiProperty({ description: '로그인 응답으로 제공되는 access token입니다.' })
   @IsJWT()
   @IsNotEmpty()
-  token: string;
+  accessToken: string;
 }
