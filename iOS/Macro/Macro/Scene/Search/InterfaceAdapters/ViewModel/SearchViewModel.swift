ef105318b95934d200f76ee0bcbee50fe8624793
Byeonjinha <jinhaday@gmail.com>
2023-11-20 19:21:10 +0900

    [feat][#34] SearchViewController 생성

---
+++ iOS/Macro/Macro/Scene/Search/InterfaceAdapters/ViewModel/SearchViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  SearchViewModel.swift
+//  Macro
+//
+//  Created by Byeon jinha on 11/20/23.
+//
+
+import Combine
+import Foundation
+
+class SearchViewModel: ViewModelProtocol {
+    
+    // MARK: - Properties
+    private var cancellables = Set<AnyCancellable>()
+    private let outputSubject = PassthroughSubject<Output, Never>()
+    
+    // MARK: - init
+    init() {
+    }
+    
+    // MARK: - Input
+    
+    enum Input {
+        case temp
+    }
+    
+    // MARK: - Output
+
+    enum Output {
+        case appleLoginCompleted
+    }
+    
+    // MARK: - Methods
+    func transform(with input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
+        input
+            .sink { [weak self] input in
+                switch input {
+                case .temp:
+                    self?.temp()
+                }
+            }
+            .store(in: &cancellables)
+        return outputSubject.eraseToAnyPublisher()
+    }
+    
+    func temp() {
+        
+    }
+}
