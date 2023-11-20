e32893292d77a098eea1bd884782b9b42bd54b67
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-20 16:24:42 +0900

    [chore] [#39] MapController 주석 해제

--- server/src/map/map.module.ts
+++ server/src/map/map.module.ts
@@ -1,14 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { KaKaoMapController } from './map.kakao.controller';
 import { KakaoMapService } from './map.kakao.service';
-// import { MapController } from './map.controller';
-// import { MapService } from './map.service';
+import { MapController } from './map.controller';
+import { MapService } from './map.service';
 
 @Global()
 @Module({
-  // controllers: [MapController],
-  // providers: [MapService],
-  controllers: [KaKaoMapController],
-  providers: [KakaoMapService],
+  controllers: [MapController, KaKaoMapController],
+  providers: [MapService, KakaoMapService],
 })
 export class MapModule {}
