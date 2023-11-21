ae8d12a4447b303169e55f285f70e5aef9350ea7
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-21 18:51:48 +0900

    [chore] lint

--- server/src/config/app.config.ts
+++ server/src/config/app.config.ts
@@ -3,7 +3,9 @@ import { load } from 'js-yaml';
 import { createSecretKey } from 'crypto';
 
 export default () => {
-  const config: Record<string, any> = load(readFileSync('application.yaml', 'utf-8'));
+  const config: Record<string, any> = load(
+    readFileSync('application.yaml', 'utf-8'),
+  );
   config.application.jwt.secret = createSecretKey(
     config.application.jwt.secret,
   ).export();
