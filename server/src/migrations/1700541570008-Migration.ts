411af8b0d38a81b1bcce9e0af577b0909e2ce62a
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-21 19:01:54 +0900

    [correct] [#54] lint

--- server/src/migrations/1700541570008-Migration.ts
+++ server/src/migrations/1700541570008-Migration.ts
@@ -1,10 +1,10 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1700541570008 implements MigrationInterface {
-    name = 'Migration1700541570008'
+  name = 'Migration1700541570008';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             CREATE TABLE \`post\` (
                 \`post_id\` int NOT NULL AUTO_INCREMENT,
                 \`title\` varchar(255) NOT NULL,
@@ -20,7 +20,7 @@ export class Migration1700541570008 implements MigrationInterface {
                 PRIMARY KEY (\`post_id\`)
             ) ENGINE = InnoDB
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE \`post_content_element\` (
                 \`post_content_element_id\` int NOT NULL AUTO_INCREMENT,
                 \`image_url\` varchar(255) NOT NULL,
@@ -31,7 +31,7 @@ export class Migration1700541570008 implements MigrationInterface {
                 PRIMARY KEY (\`post_content_element_id\`)
             ) ENGINE = InnoDB
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE \`user\` (
                 \`user_id\` varchar(36) NOT NULL,
                 \`email\` varchar(255) NOT NULL,
@@ -43,7 +43,7 @@ export class Migration1700541570008 implements MigrationInterface {
                 PRIMARY KEY (\`user_id\`)
             ) ENGINE = InnoDB
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE \`apple_auth\` (
                 \`apple_id\` varchar(255) NOT NULL,
                 \`user_id\` varchar(36) NULL,
@@ -51,7 +51,7 @@ export class Migration1700541570008 implements MigrationInterface {
                 PRIMARY KEY (\`apple_id\`)
             ) ENGINE = InnoDB
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE \`post_like_users_user\` (
                 \`post_id\` int NOT NULL,
                 \`user_email\` varchar(36) NOT NULL,
@@ -60,7 +60,7 @@ export class Migration1700541570008 implements MigrationInterface {
                 PRIMARY KEY (\`post_id\`, \`user_email\`)
             ) ENGINE = InnoDB
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE \`user_followers_relation\` (
                 \`follower_id\` varchar(36) NOT NULL,
                 \`followee_id\` varchar(36) NOT NULL,
@@ -69,7 +69,7 @@ export class Migration1700541570008 implements MigrationInterface {
                 PRIMARY KEY (\`follower_id\`, \`followee_id\`)
             ) ENGINE = InnoDB
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE \`post_ssss\` (
                 \`userUserId\` varchar(36) NOT NULL,
                 \`postPostId\` int NOT NULL,
@@ -78,117 +78,116 @@ export class Migration1700541570008 implements MigrationInterface {
                 PRIMARY KEY (\`userUserId\`, \`postPostId\`)
             ) ENGINE = InnoDB
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`post\`
             ADD CONSTRAINT \`FK_ac36ed79dc89ca03b3a630baae8\` FOREIGN KEY (\`user_email\`) REFERENCES \`user\`(\`user_id\`) ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`post_content_element\`
             ADD CONSTRAINT \`FK_588a7bf10475a4b9d6981ea1caa\` FOREIGN KEY (\`post_id\`) REFERENCES \`post\`(\`post_id\`) ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`apple_auth\`
             ADD CONSTRAINT \`FK_42f2ee24ee4e6afb73ba904fb6e\` FOREIGN KEY (\`user_id\`) REFERENCES \`user\`(\`user_id\`) ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`post_like_users_user\`
             ADD CONSTRAINT \`FK_2eac7a67b6894a42f92cc3f7cb7\` FOREIGN KEY (\`post_id\`) REFERENCES \`post\`(\`post_id\`) ON DELETE CASCADE ON UPDATE CASCADE
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`post_like_users_user\`
             ADD CONSTRAINT \`FK_de292e4c8b2344e1a2cbeaec6ac\` FOREIGN KEY (\`user_email\`) REFERENCES \`user\`(\`user_id\`) ON DELETE CASCADE ON UPDATE CASCADE
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`user_followers_relation\`
             ADD CONSTRAINT \`FK_dca93378123a3be0d10f49d03ed\` FOREIGN KEY (\`follower_id\`) REFERENCES \`user\`(\`user_id\`) ON DELETE CASCADE ON UPDATE CASCADE
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`user_followers_relation\`
             ADD CONSTRAINT \`FK_7642e3756290af9092754cda85f\` FOREIGN KEY (\`followee_id\`) REFERENCES \`user\`(\`user_id\`) ON DELETE CASCADE ON UPDATE CASCADE
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`post_ssss\`
             ADD CONSTRAINT \`FK_f43a5aa94caa7490facc1d75124\` FOREIGN KEY (\`userUserId\`) REFERENCES \`user\`(\`user_id\`) ON DELETE CASCADE ON UPDATE CASCADE
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`post_ssss\`
             ADD CONSTRAINT \`FK_918a0e6ab4a9e118699776076a7\` FOREIGN KEY (\`postPostId\`) REFERENCES \`post\`(\`post_id\`) ON DELETE CASCADE ON UPDATE CASCADE
         `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE \`post_ssss\` DROP FOREIGN KEY \`FK_918a0e6ab4a9e118699776076a7\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`post_ssss\` DROP FOREIGN KEY \`FK_f43a5aa94caa7490facc1d75124\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`user_followers_relation\` DROP FOREIGN KEY \`FK_7642e3756290af9092754cda85f\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`user_followers_relation\` DROP FOREIGN KEY \`FK_dca93378123a3be0d10f49d03ed\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`post_like_users_user\` DROP FOREIGN KEY \`FK_de292e4c8b2344e1a2cbeaec6ac\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`post_like_users_user\` DROP FOREIGN KEY \`FK_2eac7a67b6894a42f92cc3f7cb7\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`apple_auth\` DROP FOREIGN KEY \`FK_42f2ee24ee4e6afb73ba904fb6e\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`post_content_element\` DROP FOREIGN KEY \`FK_588a7bf10475a4b9d6981ea1caa\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE \`post\` DROP FOREIGN KEY \`FK_ac36ed79dc89ca03b3a630baae8\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP INDEX \`IDX_918a0e6ab4a9e118699776076a\` ON \`post_ssss\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP INDEX \`IDX_f43a5aa94caa7490facc1d7512\` ON \`post_ssss\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE \`post_ssss\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP INDEX \`IDX_7642e3756290af9092754cda85\` ON \`user_followers_relation\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP INDEX \`IDX_dca93378123a3be0d10f49d03e\` ON \`user_followers_relation\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE \`user_followers_relation\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP INDEX \`IDX_de292e4c8b2344e1a2cbeaec6a\` ON \`post_like_users_user\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP INDEX \`IDX_2eac7a67b6894a42f92cc3f7cb\` ON \`post_like_users_user\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE \`post_like_users_user\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP INDEX \`REL_42f2ee24ee4e6afb73ba904fb6\` ON \`apple_auth\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE \`apple_auth\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` ON \`user\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE \`user\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE \`post_content_element\`
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE \`post\`
         `);
-    }
-
+  }
 }
