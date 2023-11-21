ca64d07ebdb051c82b86a3c8ded4bf1cd48de5c4
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-21 18:52:24 +0900

    [chore] [#54] mysql.config option 로딩 수정

--- server/src/mysql.config.ts
+++ server/src/mysql.config.ts
@@ -2,6 +2,8 @@ import { readFileSync } from 'fs';
 import { load } from 'js-yaml';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-const options: DataSourceOptions = load(readFileSync('application.yaml', 'utf-8'))?.['migration'];
+const options: DataSourceOptions = load(
+  readFileSync('application.yaml', 'utf-8'),
+)?.['typeorm'];
 
 export default new DataSource(options);
