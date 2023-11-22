d1abf792af10f645b791f9b4fcfd83d0133ca42d
KimNahun <rlaskgns0212@naver.com>
2023-11-22 18:44:18 +0900

    [feat][#67] 장소 상세 View에서의 라벨, StackView 추가

--- iOS/Macro/Macro/Scene/LocationInfo/InterfaceAdapters/VIew/LocationInfoViewController.swift
+++ iOS/Macro/Macro/Scene/LocationInfo/InterfaceAdapters/VIew/LocationInfoViewController.swift
@@ -13,9 +13,42 @@ final class LocationInfoViewController: UIViewController {
     
     // MARK: - UI Components
     
-    private let label: UILabel = {
+    private let placeNameLabel: UILabel = {
         let label = UILabel()
         return label
+    }()
+    
+    private let addressLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private let categoryNameLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private let categoryGroupLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private let phoneLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private let horizonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.spacing = 10
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
     }()
     
     // MARK: - Life Cycle
@@ -23,6 +56,7 @@ final class LocationInfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
         setUpLayout()
     }
     
@@ -35,18 +69,26 @@ final class LocationInfoViewController: UIViewController {
 extension LocationInfoViewController {
     
     private func setTranslatesAutoresizingMaskIntoConstraints() {
-        label.translatesAutoresizingMaskIntoConstraints = false
+        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
+        horizonStackView.translatesAutoresizingMaskIntoConstraints = false
     }
     private func addsubviews() {
-        view.addSubview(label)
+        view.addSubview(horizonStackView)
+        view.addSubview(verticalStackView)
+        [categoryNameLabel, categoryGroupLabel].forEach {
+            verticalStackView.addArrangedSubview($0)
+        }
+        [placeNameLabel, addressLabel, verticalStackView, phoneLabel].forEach {
+            horizonStackView.addArrangedSubview($0)
+        }
     }
     
     private func setLayoutConstraints() {
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            label.heightAnchor.constraint(equalToConstant: 40),
+            verticalStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            verticalStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            verticalStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            verticalStackView.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
     
@@ -72,7 +114,12 @@ extension LocationInfoViewController {
 
 extension LocationInfoViewController {
     func updateText(_ model: LocationDetail) {
-        label.text = model.placeName
+        print(model)
+        placeNameLabel.text = model.placeName
+        addressLabel.text = model.addressName
+        categoryNameLabel.text = model.categoryName
+        categoryGroupLabel.text = model.categoryGroupName
+        phoneLabel.text = model.phone
     }
 }
 
