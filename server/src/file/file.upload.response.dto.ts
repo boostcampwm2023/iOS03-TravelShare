238cd74ccbf88dc5dbc2eaa445779f590ce508af
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-22 21:07:59 +0900

    [add] [#73] 응답 dto etag 추가

--- server/src/file/file.upload.response.dto.ts
+++ server/src/file/file.upload.response.dto.ts
@@ -2,7 +2,10 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsUrl } from 'class-validator';
 
 export class FileUploadResponse {
-  @ApiProperty({description: '파일 URL입니다.'})
+  @ApiProperty({ description: '파일 URL입니다.' })
   @IsUrl()
   url: string;
+
+  @ApiProperty({ description: '파일 etag' })
+  etag: string;
 }
