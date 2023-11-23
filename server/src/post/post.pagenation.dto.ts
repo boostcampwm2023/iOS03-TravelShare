0320b5736c811024fc647d333df0a6034449315e
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-23 09:44:40 +0900

    [correct] [#77] skip 수정

--- server/src/post/post.pagenation.dto.ts
+++ server/src/post/post.pagenation.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsPositive, Max } from 'class-validator';
+import { IsInt, IsOptional, IsPositive, Max, Min } from 'class-validator';
 
 export class PostPagenation {
   @ApiProperty({ description: '몇 개까지? 최대 15개' })
@@ -9,7 +9,8 @@ export class PostPagenation {
   take?: number = 10;
 
   @ApiProperty({ description: 'offset' })
-  @IsPositive()
+  @IsInt()
+  @Min(0)
   @IsOptional()
-  skip?: number = 10;
+  skip?: number = 0;
 }
