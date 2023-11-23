3afbd251331b26c5712ddae96cb22bab38824ea1
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-23 12:17:12 +0900

    [style] lint

--- server/src/entities/user.entity.ts
+++ server/src/entities/user.entity.ts
@@ -26,7 +26,7 @@ export class User {
   @Column()
   password: string;
 
-  @Column({ name: 'image_url',nullable: true })
+  @Column({ name: 'image_url', nullable: true })
   imageUrl: string;
 
   @Column('enum', { default: 'user', enum: ['user', 'admin'] })
