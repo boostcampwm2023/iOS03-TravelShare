af8dce19198aeb31e54f194938f8e505f9b40c16
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-23 20:09:16 +0900

    [feat] [#77] search api user 닉네임 로직 수정
    - 추가로 고민이 필요한 api 주석 추가

--- server/src/post/post.service.ts
+++ server/src/post/post.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Post, PostContentElement } from 'src/entities/post.entity';
-import { Like, MoreThan, Repository } from 'typeorm';
+import { Like, Raw, Repository } from 'typeorm';
 import { PostWriteBody } from './post.write.body';
 import { Transactional } from 'src/utils/transactional.decorator';
 import { PostFindQuery } from './post.find.query.dto';
@@ -21,10 +21,98 @@ export class PostService {
     private readonly postContentElementRepository: Repository<PostContentElement>,
   ) {}
 
+  /**
+   * 인기 게시글을 불러옵니다.<br>
+   * 한 달 이후 등록된 게시물 중,<br>
+   * 좋아요 수와 조회수를 기준으로 정렬하여 게시글들을 불러옵니다.<br>
+   * 이후 각 게시글마다 좋아요를 한 유저들 중 현재 로그인한 유저가 포함되어 있는지 각각 조사하여 liked를 표시합니다.<br>
+   * #####  TODO: 각 게시글에 대한 좋아요 여부를 표시할 것인가?<br>
+   * #####  최적화 필요
+   * 
+   * # 첫번째 쿼리
+   * 
+   * ```sql
+   * 
+SELECT `post`.`post_id`     AS `Post_post_id`,
+       `post`.`title`       AS `Post_title`,
+       `post`.`view_num`    AS `Post_view_num`,
+       `post`.`like_num`    AS `Post_like_num`,
+       `post`.`summary`     AS `Post_summary`,
+       `post`.`route`       AS `Post_route`,
+       `post`.`hashtag`     AS `Post_hashtag`,
+       `post`.`start_at`    AS `Post_start_at`,
+       `post`.`end_at`      AS `Post_end_at`,
+       `post`.`created_at`  AS `Post_created_at`,
+       `post`.`modified_at` AS `Post_modified_at`,
+       `post`.`deleted_at`  AS `Post_deleted_at`,
+       `post`.`user_email`  AS `Post_user_email`
+FROM   `post` `Post`
+WHERE  (( Date_add(Now(), INTERVAL -2 week) <= `post`.`created_at` ))
+       AND ( `post`.`deleted_at` IS NULL )
+ORDER  BY `post`.`like_num` DESC,
+          `post`.`view_num` DESC
+LIMIT  10 
+   * ```
+   *
+   * # 두번쨰
+   *
+   * ```sql
+SELECT `user`.`user_id`     AS `User_user_id`,
+       `user`.`email`       AS `User_email`,
+       `user`.`name`        AS `User_name`,
+       `user`.`password`    AS `User_password`,
+       `user`.`image_url`   AS `User_image_url`,
+       `user`.`role`        AS `User_role`,
+       `user`.`created_at`  AS `User_created_at`,
+       `user`.`modified_at` AS `User_modified_at`,
+       `user`.`deleted_at`  AS `User_deleted_at`
+FROM   `user` `User`
+       INNER JOIN `post` `Post`
+               ON `post`.`user_email` = `user`.`email`
+                  AND `post`.`deleted_at` IS NULL
+WHERE  ( `post`.`post_id` IN ( ?, ?, ?, ?,
+                               ?, ?, ?, ?,
+                               ?, ? ) )
+       AND ( `user`.`deleted_at` IS NULL ) -- PARAMETERS: [3,5,8,11,14,2,6,9,12,15]
+
+   * ```
+   * # 나머지 쿼리들       
+   * ```sql
+SELECT `post`.`post_id`    AS `Post_postId`,
+       `post`.`user_email` AS `User_writer_email`
+FROM   `post` `Post`
+WHERE  ( `post`.`post_id` IN ( 3, 5, 8, 11,
+                               14, 2, 6, 9,
+                               12, 15 ) )
+       AND ( `post`.`deleted_at` IS NULL );
+
+SELECT 1 AS `row_exists`
+FROM   (SELECT 1 AS dummy_column) `dummy_table`
+WHERE  EXISTS (SELECT 1
+               FROM   `post` `Post`
+                      LEFT JOIN `post_likes_users` `Post_Post__Post_likeUsers`
+                             ON
+       `Post_Post__Post_likeUsers`.`post_id` = `post`.`post_id`
+                      LEFT JOIN `user` `Post__Post_likeUsers`
+                             ON `Post__Post_likeUsers`.`email` =
+                                `Post_Post__Post_likeUsers`.`email`
+                                AND ( `Post__Post_likeUsers`.`deleted_at` IS
+                                      NULL )
+               WHERE  (( `Post__Post_likeUsers`.`email` = ? ))
+                      AND ( `post`.`deleted_at` IS NULL ))
+LIMIT  1 -- PARAMETERS: ["macro@macrocd.com"]
+
+   * ```       
+   * @param pagination
+   * @param param1
+   * @returns
+   */
   async popularList(pagination: PostHitsQuery, { email }: Authentication) {
     const posts = await this.postRepository.find({
       where: {
-        createdAt: MoreThan(new Date(Date.now() - 1000 * 60 * 60 * 24 * 30)),
+        createdAt: Raw(
+          (alias) => `DATE_ADD(NOW(), INTERVAL -2 WEEK) <= ${alias}`,
+        ),
       },
       ...(pagination ?? { take: 10 }),
       order: {
@@ -34,7 +122,9 @@ export class PostService {
       relations: {
         writer: true,
       },
+      relationLoadStrategy: 'query',
     });
+
     return plainToInstance(
       PostFindResponse,
       await Promise.all(
@@ -53,10 +143,10 @@ export class PostService {
     { email }: Authentication,
   ) {
     const posts = await this.postRepository.find({
-      where: {
-        ...(title ? { title: Like(`%${title}%`) } : {}),
-        ...(username ? { writer: { name: username } } : {}),
-      },
+      where: [
+        { ...(title ? { title: Like(`%${title}%`) } : {}) },
+        { ...(username ? { writer: Like(`%${username}%`) } : {}) },
+      ],
       ...pagination,
       relations: {
         writer: true,
@@ -79,6 +169,7 @@ export class PostService {
     return await this.postRepository.findBy({ writer: { email } });
   }
 
+  @Transactional()
   async detail({ postId }: PostDetailQuery, { email }: Authentication) {
     const post = await this.postRepository.findOneOrFail({
       where: { postId },
@@ -88,6 +179,7 @@ export class PostService {
         writer: true,
       },
     });
+    await this.postRepository.increment({ postId }, 'viewNum', 1);
     return plainToInstance(PostDetailResponse, {
       ...post,
       liked: await this.postRepository.exist({
