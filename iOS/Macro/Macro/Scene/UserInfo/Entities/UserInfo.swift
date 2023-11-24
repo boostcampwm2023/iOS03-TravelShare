ac1a631a65e29f6e0462939b4ddc10b1e9972aa4
Byeonjinha <jinhaday@gmail.com>
2023-11-24 16:52:11 +0900

    [feat][#96] UserInfoView Model 관련

---
+++ iOS/Macro/Macro/Scene/UserInfo/Entities/UserInfo.swift
@@ -0,0 +1,15 @@
+//
+//  UserInfo.swift
+//  Macro
+//
+//  Created by Byeon jinha on 11/23/23.
+//
+
+import Foundation
+
+struct UserInfo {
+    let name: String
+    var follower: Int
+    let introduce: String
+    let userImageURL: String
+}
