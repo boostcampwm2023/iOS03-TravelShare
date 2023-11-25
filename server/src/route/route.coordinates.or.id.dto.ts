253e7b6e4281236adda2c41f8c80d379be0e6d53
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-26 06:11:12 +0900

    [add] [#100] post/uplaod 용 헬퍼 dto

---
+++ server/src/route/route.coordinates.or.id.dto.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsOptional, Validate } from 'class-validator';
+import { IsCoordinate } from './route.validator.constrant';
+
+export class RouteCoordinatesOrId {
+  @ApiProperty({
+    description: '좌표 배열입니다.',
+    example: [
+      [126, 36],
+      [126.7, 35.6],
+    ],
+    required: false,
+  })
+  @Validate(IsCoordinate, { each: true })
+  @IsOptional()
+  coordinates: [number, number][];
+
+  @ApiProperty({
+    description: '고유 id입니다.',
+    example: 1,
+  })
+  @IsNumber()
+  @IsOptional()
+  routeId: number;
+}
