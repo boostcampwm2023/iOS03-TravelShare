96a35d60bed48c1b301c6c744426f6cb3b908b63
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-26 04:48:45 +0900

    [add] [#100] /post/like dto 분리

---
+++ server/src/post/post.like.response.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsOptional, Min } from 'class-validator';
+
+export class PostLikeResponse {
+  @ApiProperty({ description: '해당 게시글 좋아요 수 입니다.' })
+  @IsNumber()
+  @Min(0)
+  likeNum: number;
+
+  @ApiProperty({ description: '게시글 고유 id 입니다.' })
+  @IsNumber()
+  @Min(0)
+  @IsOptional()
+  postId: number;
+}
