1790557acc82b4e4fb603b3bf4591c7ebf88c909
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-27 18:59:13 +0900

    [chore] [#117] coordinate dto 수정 반영

--- server/src/post/post.content.element.base.dto.ts
+++ server/src/post/post.content.element.base.dto.ts
@@ -9,20 +9,9 @@ import {
   Min,
   ValidateNested,
 } from 'class-validator';
+import { RouteCoordinate } from 'src/route/route.coordinates.dto';
 
-class PostContentElementPoint {
-  @ApiProperty({ description: 'x 좌표', example: 128.0 })
-  @IsNumber()
-  @Min(124.5)
-  @Max(132.0)
-  x: number;
-
-  @ApiProperty({ description: 'y 좌표', example: 35.6 })
-  @IsNumber()
-  @Min(33.0)
-  @Max(38.9)
-  y: number;
-}
+class PostContentElementPoint extends RouteCoordinate {}
 
 export class PostContentElementBase {
   @ApiProperty({
