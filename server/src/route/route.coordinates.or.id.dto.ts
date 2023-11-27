59204fcfe90f278c05b98e46ce6128e42465011d
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-27 18:58:51 +0900

    [chore] [#117] route coordinate dto 수정

--- server/src/route/route.coordinates.or.id.dto.ts
+++ server/src/route/route.coordinates.or.id.dto.ts
@@ -1,20 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, Validate, ValidateIf } from 'class-validator';
-import { IsCoordinate } from './route.validator.constrant';
+import { IsNumber, Validate, ValidateIf, ValidateNested } from 'class-validator';
 import { LineString } from 'src/entities/route.entity';
+import { RouteCoordinate, RouteCoordinates } from './route.coordinates.dto';
+import { Type } from 'class-transformer';
 
 export class RouteCoordinatesOrId {
   @ApiProperty({
     description: '좌표 배열입니다.',
     example: [
-      [36, 122.6],
-      [35.6, 122.2],
+      {
+        x: 120,
+        y: 33.6
+      },
+      {
+        x: 127.86,
+        y: 35.8
+      }
     ],
     required: false,
   })
   @ValidateIf(({ routeId }: RouteCoordinatesOrId) => !Boolean(routeId))
-  @Validate(IsCoordinate, { each: true })
-  coordinates: LineString;
+  @ValidateNested()
+  @Type(()=> RouteCoordinate)
+  coordinates: RouteCoordinate[];
 
   @ApiProperty({
     description: '고유 id입니다.',
