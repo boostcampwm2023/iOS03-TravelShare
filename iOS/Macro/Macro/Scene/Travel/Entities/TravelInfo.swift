e3d748369fb7ea9fe664318dacd557ed9257dc9b
Byeonjinha <jinhaday@gmail.com>
2023-11-27 19:30:43 +0900

    [feat][#119] 코어데이터에 저장하기 위한 모델 작업

---
+++ iOS/Macro/Macro/Scene/Travel/Entities/TravelInfo.swift
@@ -0,0 +1,15 @@
+//
+//  TravelInfo.swift
+//  Macro
+//
+//  Created by Byeon jinha on 11/27/23.
+//
+
+import Foundation
+
+struct TravelInfo {
+    let id: String
+    let recordedLocation: [[Double]]
+    let recordedPindedLocation: [[Double]]
+    let sequence: Int16
+}
