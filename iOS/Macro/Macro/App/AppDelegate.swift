3e509ecbf024b90e2a47c585e94e1d0da053d414
Byeonjinha <jinhaday@gmail.com>
2023-11-27 19:29:29 +0900

    [feat][#119] 코어데이터 기본 세팅

--- iOS/Macro/Macro/App/AppDelegate.swift
+++ iOS/Macro/Macro/App/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by 김경호 on 11/13/23.
 //
 
+import CoreData
 import UIKit
 
 @main
@@ -17,5 +18,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+    
+    // MARK: - Core Data stack
+
+     lazy var persistentContainer: NSPersistentContainer = {
+         let container = NSPersistentContainer(name: "Model")
+         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+             if let error = error as NSError? {
+                 fatalError("Unresolved error \(error), \(error.userInfo)")
+             }
+         })
+         return container
+     }()
+
+     // MARK: - Core Data Saving support
+
+     func saveContext () {
+         let context = persistentContainer.viewContext
+         if context.hasChanges {
+             do {
+                 try context.save()
+             } catch {
+                 // Replace this implementation with code to handle the error appropriately.
+                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 let nserror = error as NSError
+                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+             }
+         }
+     }
 }
 
