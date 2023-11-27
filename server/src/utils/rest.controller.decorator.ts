a443b5e9dc2d90e2a864cab44f0edf7d96762ead
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-28 04:55:08 +0900

    [add] ValidationPipe => customDecorator 옵션 추가

--- server/src/utils/rest.controller.decorator.ts
+++ server/src/utils/rest.controller.decorator.ts
@@ -15,6 +15,12 @@ export function RestController(arg?: any) {
   return applyDecorators(
     Controller(arg),
     UseInterceptors(ResponseValidationInterceptor),
-    UsePipes(new ValidationPipe({ transform: true, whitelist: true })),
+    UsePipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        validateCustomDecorators: true,
+      }),
+    ),
   );
 }
