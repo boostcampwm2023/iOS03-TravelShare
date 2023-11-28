6b0f8bcd986bfa3ef1953bfc45c2a097a3d5ee0f
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-28 17:50:29 +0900

    [style] [#125] lint

--- server/src/user/user.follow.response.dto.ts
+++ server/src/user/user.follow.response.dto.ts
@@ -1,31 +1,31 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Type } from "class-transformer";
-import { IsEmail, IsInt, Min, ValidateNested } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsEmail, IsInt, Min, ValidateNested } from 'class-validator';
 
 export class UserFollowState {
-    @ApiProperty({description: '유저 이메일'})
-    @IsEmail()
-    email: string;
+  @ApiProperty({ description: '유저 이메일' })
+  @IsEmail()
+  email: string;
 
-    @ApiProperty({description: '팔로워 숫자'})
-    @IsInt()
-    @Min(0)
-    followersNum: number;
+  @ApiProperty({ description: '팔로워 숫자' })
+  @IsInt()
+  @Min(0)
+  followersNum: number;
 
-    @ApiProperty({description: '팔로잉 숫자'})
-    @IsInt()
-    @Min(0)
-    followeesNum: number;
+  @ApiProperty({ description: '팔로잉 숫자' })
+  @IsInt()
+  @Min(0)
+  followeesNum: number;
 }
 
 export class UserFollowResponse {
-    @ApiProperty({description: '팔로잉'})
-    @ValidateNested()
-    @Type(()=> UserFollowState)
-    followee: UserFollowState;
+  @ApiProperty({ description: '팔로잉' })
+  @ValidateNested()
+  @Type(() => UserFollowState)
+  followee: UserFollowState;
 
-    @ApiProperty({description: '팔로워'})
-    @ValidateNested()
-    @Type(()=> UserFollowState)
-    follower: UserFollowState;
+  @ApiProperty({ description: '팔로워' })
+  @ValidateNested()
+  @Type(() => UserFollowState)
+  follower: UserFollowState;
 }
