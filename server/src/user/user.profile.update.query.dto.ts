c45c74dbf52826aa01f4f647e4896b7123fd21ae
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-28 16:32:51 +0900

    [chore] [#125] /update dto 수정

--- server/src/user/user.profile.update.query.dto.ts
+++ server/src/user/user.profile.update.query.dto.ts
@@ -2,8 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString, IsUrl } from 'class-validator';
 
 export class UserProfileUpdateQuery {
-  @ApiProperty({ description: '이름' })
+  @ApiProperty({ description: '이름', required: false })
   @IsString()
+  @IsOptional()
   name?: string;
 
   @ApiProperty({ description: '이미지 url', required: false })
