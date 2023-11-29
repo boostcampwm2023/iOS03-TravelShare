a417ba482cfdcb568ea6da27111cbc475612039d
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-29 16:28:01 +0900

    [add] [#137] place dto 추가

---
+++ server/src/post/place.base.dto.ts
@@ -0,0 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsNumberString,
+  IsPhoneNumber,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { RouteCoordinate } from 'route/route.coordinates.dto';
+
+export class PlaceBase {
+  @ApiProperty({ description: '장소 고유 id입니다.', example: '1234567' })
+  @IsNumberString()
+  placeId: string;
+
+  @ApiProperty({ description: '장소 이름입니다.', example: '어디어디가게' })
+  @IsString()
+  placeName: string;
+
+  @ApiProperty({ description: '매장 번호입니다.', example: '01012345678' })
+  @IsPhoneNumber('KR')
+  phoneNumber: string;
+
+  @ApiProperty({
+    description: '매장 카테고리입니다.',
+    example: '무엇 > 무엇 > 무엇',
+  })
+  @IsString()
+  category: string;
+
+  @ApiProperty({ description: '장소 주소', example: '어디동 ' })
+  @IsString()
+  address: string;
+
+  @ApiProperty({ description: '도로명 주소입니다.', example: '어디길' })
+  @IsString()
+  roadAddress: string;
+
+  @ApiProperty({ description: '핑 좌표' })
+  @ValidateNested()
+  @Type(() => RouteCoordinate)
+  coordinate: RouteCoordinate;
+}
