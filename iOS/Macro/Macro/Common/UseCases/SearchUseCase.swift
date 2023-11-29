90593b32f3d4bd5992eea384c73214c90491632b
KimNahun <rlaskgns0212@naver.com>
2023-11-29 11:47:52 +0900

    [chore][#135] SeachUseCase 내에 주석 추가

--- iOS/Macro/Macro/Common/UseCases/SearchUseCase.swift
+++ iOS/Macro/Macro/Common/UseCases/SearchUseCase.swift
@@ -10,12 +10,25 @@ import Foundation
 import MacroNetwork
 
 protocol SearchUseCase {
+    /// 특정 장소를 검색했을 때 받아오는 검색 결과
     func searchLocation(query: String, page: Int) -> AnyPublisher<[LocationDetail], NetworkError>
+    
+    /// 특정 검색어가 제목에 포함되었는지를 검색하는 결과
     func searchPost(query: String) -> AnyPublisher<[PostFindResponse], NetworkError>
+    
+    /// 특정 검색어가 해당 json 파일 list의 제목에 포함되는지 검색
     func searchMockPost(query: String, json: String) -> AnyPublisher<[PostFindResponse], NetworkError>
+    
+    /// 가장 인기있는 게시물들을 받아오는 작업
     func fetchHitPost() -> AnyPublisher<[PostFindResponse], NetworkError>
+    
+    /// 홈 화면의 게시글들을 해당 json 파일에서 모두 불러오는 작업
     func searchMockPost(json: String) -> AnyPublisher<[PostFindResponse], NetworkError>
-    func searchUser(query: String) -> AnyPublisher<ProfileGetResponse, NetworkError>
+    
+    /// 특정 사용자의 id를 검색하면, 해당 사용자의 게시글들을 모두 불러옴
+    func searchUserProfile(query: String) -> AnyPublisher<ProfileGetResponse, NetworkError>
+    
+    /// 특정 사용자의 id를 검색하면, 해당 json 파일 내의 해당 사용자의 게시글 모두 불러옴
     func searchMockUserProfile(query: String, json: String) -> AnyPublisher<ProfileGetResponse, NetworkError>
 }
 
@@ -52,7 +65,7 @@ final class Searcher: SearchUseCase {
         return provider.mockRequest(PostEndPoint.search, url: json)
     }
     
-    func searchUser(query: String) -> AnyPublisher<ProfileGetResponse, MacroNetwork.NetworkError> {
+    func searchUserProfile(query: String) -> AnyPublisher<ProfileGetResponse, MacroNetwork.NetworkError> {
         return provider.request(UserInfoEndPoint.search(query))
     }
     
