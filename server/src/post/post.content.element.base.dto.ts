0d30429ba7213de59785ad19fb5519b66f93f1bd
JIJIHUNY <dlwlgns2792@gmail.com>
2023-11-30 14:28:25 +0900

    [correct] [#159] postContent

--- server/src/post/post.content.element.base.dto.ts
+++ server/src/post/post.content.element.base.dto.ts
@@ -8,19 +8,22 @@ class PostContentElementPoint extends RouteCoordinate {}
 export class PostContentElementBase {
   @ApiProperty({
     description: '이미지 url',
-    required: false,
     example:
       'https://kr.object.ncloudstorage.com/macro-bucket/static/image/boostcampmacro-beeae45f-3772-427c-ab71-bf85b663b043',
   })
   @IsUrl()
   imageUrl: string;
 
-  @ApiProperty({ description: '설명', example: '이곳에 설명이 들어갑니다.' })
+  @ApiProperty({
+    description: '설명',
+    example: '이곳에 설명이 들어갑니다.',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   description: string;
 
-  @ApiProperty({ description: '좌표입니다.' })
+  @ApiProperty({ description: '좌표입니다.', required: false })
   @ValidateNested()
   @Type(() => PostContentElementPoint)
   @IsOptional()
