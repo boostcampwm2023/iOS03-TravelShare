a1d0f32cc66fffb931bb8ae7cb66185037b2be95
KimNahun <rlaskgns0212@naver.com>
2023-12-01 12:08:02 +0900

    [feat] 키보드 숨기는 부분 util로 빼고, mypage에서도 사용할 수 있도록 구현

--- iOS/Macro/Macro/Scene/Write/InterfaceAdapters/View/WriteViewController.swift
+++ iOS/Macro/Macro/Scene/Write/InterfaceAdapters/View/WriteViewController.swift
@@ -320,15 +320,3 @@ extension WriteViewController: UITextFieldDelegate, UITextViewDelegate {
     }
 }
 
-// 키보드 숨기기
-extension UIViewController {
-    func hideKeyboardWhenTappedAround() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-    
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-}
