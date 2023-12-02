c2c0f79fbf3e2a1b26a776a7d3f8636e3da64ae5
JIJIHUNY <dlwlgns2792@gmail.com>
2023-12-03 04:04:15 +0900

    [chore] internalServerError

--- server/src/utils/response.validation.interceptor.ts
+++ server/src/utils/response.validation.interceptor.ts
@@ -2,6 +2,8 @@ import {
   CallHandler,
   ExecutionContext,
   Injectable,
+  InternalServerErrorException,
+  Logger,
   NestInterceptor,
 } from '@nestjs/common';
 import { validateOrReject } from 'class-validator';
@@ -9,30 +11,39 @@ import { Observable, map } from 'rxjs';
 
 @Injectable()
 export class ResponseValidationInterceptor implements NestInterceptor {
+  private readonly logger: Logger = new Logger(
+    ResponseValidationInterceptor.name,
+  );
+
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> {
     return next.handle().pipe(
       map(async (value) => {
-        if (!value || typeof value !== 'object') {
+        try {
+          if (!value || typeof value !== 'object') {
+            return value;
+          }
+          if (value instanceof Promise) {
+            value.then(
+              async (future) =>
+                await validateOrReject(future, { whitelist: true }),
+            );
+          } else if (Array.isArray(value)) {
+            await Promise.all(
+              value.map(async (each) => {
+                await validateOrReject(each, { whitelist: true });
+              }),
+            );
+          } else {
+            await validateOrReject(value, { whitelist: true });
+          }
           return value;
+        } catch (err) {
+          this.logger.error(err);
+          throw new InternalServerErrorException(err?.[0]?.constraints ?? err);
         }
-        if (value instanceof Promise) {
-          value.then(
-            async (future) =>
-              await validateOrReject(future, { whitelist: true }),
-          );
-        } else if (Array.isArray(value)) {
-          await Promise.all(
-            value.map(async (each) => {
-              await validateOrReject(each, { whitelist: true });
-            }),
-          );
-        } else {
-          await validateOrReject(value, { whitelist: true });
-        }
-        return value;
       }),
     );
   }
