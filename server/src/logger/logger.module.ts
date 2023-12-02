825d59bd3603972f0b4bff0f0350dd6cb1924dec
JIJIHUNY <dlwlgns2792@gmail.com>
2023-12-03 04:02:59 +0900

    [chore] 로거 의존성

--- server/src/logger/logger.module.ts
+++ server/src/logger/logger.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { ConsoleLogger, Module } from '@nestjs/common';
 import { NcpEffectiveLogSearchAnalyticsConfigFactory } from './ncp.elsa.logger.config.factory';
 import { NcpEffectiveLogSearchAnalyticsLogger } from './ncp.elsa.logger.provider';
 import { ConfigService } from '@nestjs/config';
 import { NcpEffectiveLogSearchAnalyticsConfig } from './ncp.elsa.config.dto';
+import { APPLICATION_LOGGER_SYMBOL } from './app.logger.symbol';
 
 @Module({
   providers: [
@@ -11,8 +12,14 @@ import { NcpEffectiveLogSearchAnalyticsConfig } from './ncp.elsa.config.dto';
       provide: NcpEffectiveLogSearchAnalyticsConfig,
       useFactory: NcpEffectiveLogSearchAnalyticsConfigFactory,
     },
-    NcpEffectiveLogSearchAnalyticsLogger,
+    {
+      provide: APPLICATION_LOGGER_SYMBOL,
+      useClass:
+        process.env.NODE_ENV === 'production'
+          ? NcpEffectiveLogSearchAnalyticsLogger
+          : ConsoleLogger,
+    },
   ],
-  exports: [NcpEffectiveLogSearchAnalyticsLogger],
+  exports: [APPLICATION_LOGGER_SYMBOL],
 })
 export class LoggerModule {}
