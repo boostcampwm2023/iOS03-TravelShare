da76bac6ec59ecc9ae92227ed019ccb4f6b51abe
KimNahun <rlaskgns0212@naver.com>
2023-12-05 04:12:47 +0900

    [feat][#188] 유저 검색 후 보여줄 CollectionView 임시 구현

---
+++ iOS/Macro/Macro/Scene/Search/InterfaceAdapters/View/UserResultCell.swift
@@ -0,0 +1,8 @@
+//
+//  UserResultCell.swift
+//  Macro
+//
+//  Created by 김나훈 on 12/5/23.
+//
+
+import Foundation
---
+++ iOS/Macro/Macro/Scene/Search/InterfaceAdapters/View/UserResultCollectionView.swift
@@ -0,0 +1,8 @@
+//
+//  UserResultCollectionView.swift
+//  Macro
+//
+//  Created by 김나훈 on 12/5/23.
+//
+
+import Foundation
