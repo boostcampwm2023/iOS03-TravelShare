5695386cfbecd86d81888992bf758d6026ba3a5a
Byeonjinha <jinhaday@gmail.com>
2023-12-05 03:23:42 +0900

    [feat][#189] CoreData Entity 추가 및 Relationship 설정

--- iOS/Macro/Macro/Util/CoreData/CoreDataManager.swift
+++ iOS/Macro/Macro/Util/CoreData/CoreDataManager.swift
@@ -15,21 +15,37 @@ final class CoreDataManager {
     var coreDataStack = CoreDataStack(modelName: Label.modelName)
     
     func fetchTravel(completion: @escaping ([TravelInfo]?) -> Void) throws {
-        let request = NSFetchRequest<Travel>(entityName: "Travel")
+        let request = NSFetchRequest<TravelMO>(entityName: EntityName.travel)
         do {
-            var travels = try coreDataStack.managedContext.fetch(request)
-            travels.sort { $0.sequence > $1.sequence }
+            let sort = NSSortDescriptor(key: Label.sequence, ascending: false)
+            request.sortDescriptors = [sort]
+            let travels = try coreDataStack.managedContext.fetch(request)
+
             let travelInfoArray: [TravelInfo] = try travels.map({
                 guard let id = $0.id,
                       let recordedLocation = $0.recordedLocation,
-                      let recordedPindedLocation = $0.recordedPindedLocation
+                      let recordedPinnedLocations = $0.recordedPinnedLocations
                 else {
-                    throw CoreDataError.intputError("d")
+                    throw CoreDataError.intputError("mapping error")
+                }
+                
+                guard let pinnedLocationInfo = recordedPinnedLocations.array as? [RecordedPinnedLocationMO] else { throw CoreDataError.intputError("mapping error") }
+     
+                let recordedPinnedLocationInfomationArray: [RecordedPinnedLocationInfomation] = pinnedLocationInfo.map {
+                    RecordedPinnedLocationInfomation(
+                        placeId: $0.placeId,
+                        placeName: $0.placeName,
+                        phoneNumber: $0.phoneNumber,
+                        category: $0.category,
+                        address: $0.address,
+                        roadAddress: $0.roadAddress,
+                        coordinate: PinnedLocation(latitude: $0.coordinate?[0] ?? 0, longitude: $0.coordinate?[1] ?? 0)
+                    )
                 }
                 
                 return TravelInfo(id: id,
                                   recordedLocation: recordedLocation,
-                                  recordedPindedInfo: recordedPindedLocation,
+                                  recordedPinnedLocations: recordedPinnedLocationInfomationArray,
                                   sequence: Int($0.sequence),
                                   startAt: $0.startAt,
                                   endAt: $0.endAt)
@@ -42,21 +58,40 @@ final class CoreDataManager {
     
     func saveTravel(id: String,
                     recordedLocation: [[Double]],
-                    recordedPindedLocation: [[String: [Double]]]?,
+                    recordedPinnedLocations: [RecordedPinnedLocationInfomation]?,
                     sequence: Int,
                     startAt: Date,
                     endAt: Date) {
-        let request = NSFetchRequest<Travel>(entityName: Label.entityName)
+        
+        let request = NSFetchRequest<TravelMO>(entityName: EntityName.travel)
+        
+        let travelObject = NSEntityDescription.insertNewObject(forEntityName: EntityName.travel, into: coreDataStack.managedContext)
+        travelObject.setValue(id, forKey: Label.id)
+        travelObject.setValue(recordedLocation, forKey: Label.recordedLocation)
+        travelObject.setValue(sequence, forKey: Label.sequence)
+        travelObject.setValue(startAt, forKey: Label.startAt)
+        travelObject.setValue(endAt, forKey: Label.endAt)
+        
+        if let recordedPinnedLocations = recordedPinnedLocations {
+            for recordedPinnedLocation in recordedPinnedLocations {
+                
+                guard let recordedPinnedLocationObejct = NSEntityDescription.insertNewObject(forEntityName: EntityName.recordedPinnedLocation, into: coreDataStack.managedContext) as? RecordedPinnedLocationMO else { return }
+                recordedPinnedLocationObejct.id = id
+                recordedPinnedLocationObejct.category = recordedPinnedLocation.category
+                if let recordedPinnedCoordinate = recordedPinnedLocation.coordinate { recordedPinnedLocationObejct.coordinate = [recordedPinnedCoordinate.latitude, recordedPinnedCoordinate.longitude]
+                }
+                recordedPinnedLocationObejct.address = recordedPinnedLocation.address
+                recordedPinnedLocationObejct.phoneNumber =  recordedPinnedLocation.phoneNumber
+                recordedPinnedLocationObejct.placeId = recordedPinnedLocation.placeId
+                recordedPinnedLocationObejct.placeName = recordedPinnedLocation.placeName
+                recordedPinnedLocationObejct.roadAddress = recordedPinnedLocation.roadAddress
+                
+                (travelObject as? TravelMO)?.addToRecordedPinnedLocations(recordedPinnedLocationObejct)
+            }
+        }
         
         do {
             _ = try coreDataStack.managedContext.fetch(request)
-            let travel = NSEntityDescription.insertNewObject(forEntityName: Label.entityName, into: coreDataStack.managedContext)
-            travel.setValue(id, forKey: Label.id)
-            travel.setValue(recordedLocation, forKey: Label.recordedLocation)
-            travel.setValue(recordedPindedLocation, forKey: Label.recordedPindedLocation)
-            travel.setValue(sequence, forKey: Label.sequence)
-            travel.setValue(startAt, forKey: Label.startAt)
-            travel.setValue(endAt, forKey: Label.endAt)
             coreDataStack.saveContext()
         } catch {
             Log.make().error("\(error)")
@@ -64,12 +99,12 @@ final class CoreDataManager {
     }
     
     func deleteTravel(travelUUID: String) {
-        let request = NSFetchRequest<Travel>(entityName: "Travel")
+        let request = NSFetchRequest<TravelMO>(entityName: EntityName.travel)
         do {
             let travels = try coreDataStack.managedContext.fetch(request)
             for travel in travels where travel.id == travelUUID {
-                    self.coreDataStack.managedContext.delete(travel)
-                    break
+                self.coreDataStack.managedContext.delete(travel)
+                break
             }
         } catch {
         }
@@ -77,16 +112,16 @@ final class CoreDataManager {
     }
     
     func calculateNextSequence() throws -> Int {
-        let request = NSFetchRequest<Travel>(entityName: "Travel")
+        let request = NSFetchRequest<TravelMO>(entityName: EntityName.travel)
         request.propertiesToFetch = ["sequence"]
-        request.sortDescriptors = [NSSortDescriptor(key: "sequence", ascending: false)] // sequence 내림차순으로 정렬
-
+        request.sortDescriptors = [NSSortDescriptor(key: "sequence", ascending: false)]
+        
         do {
             let travels = try coreDataStack.managedContext.fetch(request)
             if let maxSequence = travels.first?.sequence {
-                return Int(maxSequence) + 1 // 다음 시퀀스는 최대값 + 1
+                return Int(maxSequence) + 1
             } else {
-                return 1 // 시퀀스가 없을 경우 1부터 시작
+                return 1
             }
         } catch {
             throw CoreDataError.intputError("Failed to calculate next sequence")
@@ -99,14 +134,18 @@ final class CoreDataManager {
 extension CoreDataManager {
     enum Label {
         static let modelName: String = "Macro"
-        static let entityName: String = "Travel"
         
         static let id: String = "id"
         static let recordedLocation: String = "recordedLocation"
-        static let recordedPindedLocation: String = "recordedPindedLocation"
+        static let recordedPinnedLocation: String = "recordedPinnedLocation"
         static let sequence: String = "sequence"
         static let startAt: String = "startAt"
         static let endAt: String = "endAt"
+    }
+    
+    enum EntityName {
+        static let travel: String = "Travel"
+        static let recordedPinnedLocation: String = "RecordedPinnedLocation"
     }
 }
 
---
+++ iOS/Macro/RecordedPinnedLocationMO+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedPinnedLocationMO+CoreDataClass.swift
+//  
+//
+//  Created by Byeon jinha on 12/4/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(RecordedPinnedLocationMO)
+public class RecordedPinnedLocationMO: NSManagedObject {
+
+}
