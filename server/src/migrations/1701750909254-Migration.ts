1b4df8292b8fcbf154ed212755d437abe8e72046
JIJIHUNY <dlwlgns2792@gmail.com>
2023-12-05 14:10:59 +0900

    [add] migration

---
+++ server/src/migrations/1701750909254-Migration.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Migration1701750909254 implements MigrationInterface {
+  name = 'Migration1701750909254';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE \`post\`
+            ADD \`score\` int NOT NULL DEFAULT '0'
+        `);
+    await queryRunner.query(`
+            CREATE INDEX \`IDX_fdd04e5822a194b88c05be6960\` ON \`post\` (\`score\`)
+        `);
+    await queryRunner.query(`
+            ALTER TABLE \`post\`
+            DROP COLUMN \`view_num\`
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            DROP INDEX \`IDX_fdd04e5822a194b88c05be6960\` ON \`post\`
+        `);
+    await queryRunner.query(`
+            ALTER TABLE \`post\` DROP COLUMN \`score\`
+        `);
+    await queryRunner.query(`
+            ALTER TABLE \`post\`
+            ADD COLUMN \`view_num\`
+        `);
+  }
+}
