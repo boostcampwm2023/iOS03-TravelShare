b8a3ff9e398a9f015ed3bbe935e5e5e29af74450
Byeonjinha <jinhaday@gmail.com>
2023-12-10 09:33:00 +0900

    [feat][#272] 뷰타입 추가

---
+++ iOS/Macro/Macro/Common/TabBar/Model/TabbarType.swift
@@ -0,0 +1,16 @@
+//
+//  TabbarType.swift
+//  Macro
+//
+//  Created by Byeon jinha on 12/10/23.
+//
+
+import Foundation
+
+enum TabbarType {
+    case home
+    case travel
+    case myPage
+    case search
+    case write
+}
