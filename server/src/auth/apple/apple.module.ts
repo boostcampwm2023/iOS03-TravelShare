a60b283c68072c1d8d1cd24c77bbcd596729af5c
JIJIHUNY <dlwlgns2792@gmail.com>
2023-12-11 03:07:09 +0900

    [add] [#278] softDelete를 위한 post dependency

--- server/src/auth/apple/apple.module.ts
+++ server/src/auth/apple/apple.module.ts
@@ -4,9 +4,10 @@ import { AppleAuthService } from './apple.auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppleAuth } from 'entities/apple.auth.entity';
 import { User } from 'entities/user.entity';
+import { Post } from 'entities/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AppleAuth, User])],
+  imports: [TypeOrmModule.forFeature([AppleAuth, User, Post])],
   controllers: [AppleAuthController],
   providers: [AppleAuthService],
 })
