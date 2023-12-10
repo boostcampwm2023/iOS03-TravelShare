bd12eefc805f626429efd135861633ad649ec33a
KimNahun <rlaskgns0212@naver.com>
2023-12-10 09:00:23 +0900

    [feat][#244] pin 한 장소를 터치하면, 해당 장소로 카메라가 이동하도록 구현

--- iOS/Macro/Macro/Scene/Travel/InterfaceAdapters/View/RouteModalViewController.swift
+++ iOS/Macro/Macro/Scene/Travel/InterfaceAdapters/View/RouteModalViewController.swift
@@ -175,7 +175,23 @@ extension RouteModalViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = viewModel.savedRoute.pinnedPlaces[indexPath.row].placeName
+        cell.textLabel?.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTextLabelTap(_:)))
+        cell.textLabel?.addGestureRecognizer(tapGesture)
+        
         return cell
+    }
+    
+    @objc private func handleTextLabelTap(_ gesture: UITapGestureRecognizer) {
+        guard let label = gesture.view as? UILabel,
+              let cell = label.superview?.superview as? UITableViewCell,
+              let indexPath = tableView.indexPath(for: cell) else {
+            return
+        }
+        
+        let locationInfo = viewModel.savedRoute.pinnedPlaces[indexPath.row]
+        guard let mapX = Double(locationInfo.mapx), let mapY = Double(locationInfo.mapy) else { return }
+        inputSubject.send(.selectSearchedcell(mapX, mapY))
     }
     
     func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
@@ -200,11 +216,5 @@ extension RouteModalViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let locationInfo = viewModel.savedRoute.pinnedPlaces[indexPath.row]
-        guard let mapX = Double(locationInfo.mapx) else { return }
-        guard let mapY = Double(locationInfo.mapy) else { return }
-        inputSubject.send(.selectSearchedcell(mapX, mapY))
     }
 }
