6c81c0a366c390e295220b4270691608925ed0b7
ykm989 <ykm989@naver.com>
2024-01-10 21:36:31 +0900

    [chore][#338] ImageSaveManager 싱글 데이터 업로드 코드 생성

--- iOS/Macro/Macro/Infrastructure/PersistentStorages/ImageSaveManager.swift
+++ iOS/Macro/Macro/Infrastructure/PersistentStorages/ImageSaveManager.swift
@@ -10,9 +10,31 @@ import Foundation
 import MacroNetwork
 
 final class ImageSaveManager {
+    static let shared = ImageSaveManager()
+    private init() {}
     private var cancellables = Set<AnyCancellable>()
     
-    func convertImageDataToImageURL(imageDatas: [Data], completion: @escaping (([String]) -> Void)) {
+    // 이미지 데이터가 한개 인 경우
+    func convertImageDataToImageURL(imageData: Data, completion: @escaping ((String) -> Void)) {
+        let provider = APIProvider(session: URLSession.shared)
+        let uploadImageUseCase = UploadImage(provider: provider)
+        
+        uploadImageUseCase.execute(imageData: imageData)
+            .receive(on: DispatchQueue.global())
+            .sink { result in
+                if case let .failure(error) = result {
+                    debugPrint("Image Upload Fail : ", error)
+                }
+            } receiveValue: { imageURLResponse in
+                if !imageURLResponse.url.isEmpty {
+                    completion(imageURLResponse.url)
+                }
+            }
+            .store(in: &self.cancellables)
+    }
+    
+    // Image Data가 여러개 있을 경우
+    func convertImageDatasToImageURLs(imageDatas: [Data], completion: @escaping (([String]) -> Void)) {
         guard !imageDatas.isEmpty else {
             completion([])
             return
